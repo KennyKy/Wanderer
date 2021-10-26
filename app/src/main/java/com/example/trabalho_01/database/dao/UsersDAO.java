3fdc2917bbcee2868b1f2bcefdec60ead57a2a2c
Arthur Fernandes <arthurbpfernandes@gmail.com>
2021-10-26 20:12:55 -0300

    Add simple database logic

---
+++ app/src/main/java/com/example/trabalho_01/database/dao/UsersDAO.java
@@ -0,0 +1,112 @@
+package com.example.trabalho_01.database.dao;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+
+import com.example.trabalho_01.database.DbOpenHelper;
+import com.example.trabalho_01.database.model.User;
+
+public class UsersDAO extends AbstractDAO {
+    private final String[]
+            columns = {
+            User.EMAIL_COLUMN,
+            User.USERNAME_COLUMN,
+            User.PASSWORD_COLUMN,
+    };
+
+    public UsersDAO(final Context context) {
+        db_helper = new DbOpenHelper(context);
+    }
+
+    public long Insert(User model) {
+        long numberOfRows;
+
+        try {
+            Open();
+            ContentValues values = new ContentValues();
+            values.put(User.EMAIL_COLUMN, model.getEmail());
+            values.put(User.USERNAME_COLUMN, model.getUsername());
+            values.put(User.PASSWORD_COLUMN, model.getPassword());
+            numberOfRows = db.insert(User.TABLE_NAME, null, values);
+        } catch (Error e) {
+            numberOfRows = 0;
+        } finally {
+            Close();
+        }
+
+        return numberOfRows;
+    }
+
+    public int Delete() {
+        return 0;
+    }
+
+    public int Update() {
+        return 0;
+    }
+
+    //- Select da validação do login.
+    public User VerifyLogin(final String username, final String password) {
+
+        User model = null;
+        try {
+            Open();
+            Cursor cursor = db.query(
+                    User.TABLE_NAME,
+                    columns,
+                    User.USERNAME_COLUMN + " = ? and " + User.PASSWORD_COLUMN + " = ?",
+                    new String[]{username, password},
+                    null,
+                    null,
+                    null); //
+
+            cursor.moveToFirst();
+
+            while (!cursor.isAfterLast()) {
+                model = CursorToStructure(cursor);
+                break;
+            }
+        } catch (Error e) {
+
+        } finally {
+            Close();
+        }
+        return model;
+    }
+
+    public User Select(final String username) {
+        User model = null;
+
+        try {
+            Open();
+            Cursor cursor = db.query(
+                    User.TABLE_NAME,
+                    columns,
+                    User.USERNAME_COLUMN + " = ?",
+                    new String[]{username},
+                    null,
+                    null,
+                    null); //
+
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                model = CursorToStructure(cursor);
+                break;
+            }
+        } catch (Error e) {
+
+        } finally {
+            Close();
+        }
+        return model;
+    }
+
+    public final User CursorToStructure(Cursor cursor) {
+        User model = new User();
+        model.setEmail(cursor.getString(1));
+        model.setPassword(cursor.getString(2));
+        model.setUsername(cursor.getString(3));
+        return model;
+    }
+}
---
+++ app/src/main/java/com/example/trabalho_01/database/model/User.java
@@ -0,0 +1,43 @@
+package com.example.trabalho_01.database.model;
+
+public class User {
+    public static final String
+        TABLE_NAME="users",
+        USERNAME_COLUMN="username",
+        EMAIL_COLUMN="email",
+        PASSWORD_COLUMN="password";
+
+    public static final String
+        CREATE_TABLE = "CREATE TABLE users (username, email, password);";
+
+    public static final String
+        DROP_TABLE = " drop table if exists users";
+
+    private String username;
+    private String email;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
